2a4c2bf994942684e4bfa6995ebd8b97a6a44dae
Jiteshiyu <109058809+Jiteshiyu@users.noreply.github.com>
2024-09-28 19:51:37 +0530

    Update Gallery.jsx

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -3,21 +3,22 @@ import ImageGrid from "./ImageGrid.jsx";
 import Uploader from "./Uploader.jsx";
 
 export default function Gallery() {
+  // State to hold the array of image URLs
   const [images, setImages] = useState([
-    // Initial set of images (URLs)
     "../bird.jpg",
     "../mountain.jpg",
     "../bear.jpg",
   ]);
 
+  // Function to add a new image URL to the state
   const addImage = (url) => {
     setImages([...images, url]);
   };
 
   return (
     <div>
-      <Uploader addImage={addImage} />
-      <ImageGrid images={images} />
+      <Uploader addImage={addImage} /> {/* Component for uploading images */}
+      <ImageGrid images={images} /> {/* Component to display the grid of images */}
     </div>
   );
 }
