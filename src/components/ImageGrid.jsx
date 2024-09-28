7a3d43667a90a45d8e3f051f7cadced3dbf02787
Jiteshiyu <109058809+Jiteshiyu@users.noreply.github.com>
2024-09-28 19:52:07 +0530

    Update ImageGrid.jsx

--- src/components/ImageGrid.jsx
+++ src/components/ImageGrid.jsx
@@ -2,9 +2,9 @@ import ImageItem from "./ImageItem.jsx";
 
 export default function ImageGrid({ images }) {
   return (
-    <div className="img-grid">
+    <div className="img-grid"> {/* Container for the image grid */}
       {images.map((img, index) => (
-        <ImageItem key={index} image={img} />
+        <ImageItem key={index} image={img} /> // Render each ImageItem component
       ))}
     </div>
   );
